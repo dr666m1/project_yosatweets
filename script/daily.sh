eb0eb4fb58a8d03c9f42f8776707d89d2e06027f
Dairyo <sekinedairyou@gmail.com>
2019-09-19 17:40:31 +0900

    aaaaaaa

---
+++ script/daily.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+chdir $(dirname $0)
+if [ $(date '+%w') -eq 1 ]; then
+    rm ./../data/last_week.db
+    mv ./../data/this_week.db ./../data/last_week.db
+    $HOME/anaconda3/envs/yosatweets/bin/python3 ./count_tweets.py > ./../log/count_tweets.log 2>&1
+elif [ $(date '+%w') -eq 3 ]; then
+    $HOME/anaconda3/envs/yosatweets/bin/python3 ./plot_wordcloud.py > ./../log/plot_wordcloud.log 2>&1
+fi
+
---
+++ script/hourly.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chdir $(dirname $0)
+$HOME/anaconda3/envs/yosatweets/bin/python3 ./insert_tweets.py > ./../log/insert_tweets_$(date '+%H').log 2>&1
