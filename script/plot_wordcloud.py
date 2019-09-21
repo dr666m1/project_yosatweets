deba27348d0f2d782e0c2105df353797b4f988f4
Dairyo <sekinedairyou@gmail.com>
2019-09-21 03:54:42 +0000

    janome -> mecab

--- script/plot_wordcloud.py
+++ script/plot_wordcloud.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
-from janome.tokenizer import Tokenizer
-#import MeCab
+#from janome.tokenizer import Tokenizer
+import MeCab
 import common
 import pandas as pd
 from wordcloud import WordCloud
@@ -13,34 +13,36 @@ except NameError as e:
 with common.make_conn("./../data/last_week.db") as conn:
     df=pd.read_sql_query("select * from tweets;",conn)
 
-t=Tokenizer("./../dic/user_dic.csv",udic_type="simpledic",udic_enc="utf8")
+#t=Tokenizer("./../dic/user_dic.csv",udic_type="simpledic",udic_enc="utf8")
+#for txt in df["text"]:
+#    converted_txt=common.apply_convert_dic(txt)
+#    for token in t.tokenize(converted_txt):
+#        if token.base_form in ignore_words:
+#            continue
+#        elif token.part_of_speech.split(",")[0] in ["名詞","形容詞","動詞"]:
+#            if token.base_form in word_freq.keys():
+#                word_freq[token.base_form]+=1
+#            else:
+#                word_freq[token.base_form]=1
+
 word_freq={}
 ignore_words=common.read_ignore_dic()
+chasen=MeCab.Tagger("-Ochasen")
 for txt in df["text"]:
-    converted_txt=common.apply_convert_dic(txt)
-    for token in t.tokenize(converted_txt):
-        if token.base_form in ignore_words:
-            continue
-        elif token.part_of_speech.split(",")[0] in ["名詞","形容詞","動詞"]:
-            if token.base_form in word_freq.keys():
-                word_freq[token.base_form]+=1
-            else:
-                word_freq[token.base_form]=1
+    node=chasen.parseToNode(txt)
+    while node is not None:
+        word=node.feature.split(",")[6]
+        if word in ignore_words:
+            pass
+        elif node.feature.split(",")[0] not in ["名詞","形容詞","動詞"]:
+            pass
+        elif word in word_freq.keys():
+            word_freq[word]+=1
+        else:
+            word_freq[word]=1
+        node=node.next
 
-#chasen=MeCab.Tagger("-Ochasen")
-#node=chasen.parseToNode(df["text"][0])
-#for txt in df["text"]:
-#    node=chasen.parseToNode(txt)
-#    while node is not None:
-#        word=node.feature.split(",")[6]
-#        if node.feature.split(",")[0] in ["名詞","形容詞","動詞"]:
-#            if word in word_freq.keys():
-#                word_freq[word]+=1
-#            else:
-#                word_freq[word]=1
-#        node=node.next
-
-wc=WordCloud(font_path="./../font/YuGothB.ttc",width=1200,height=600)
+wc=WordCloud(font_path="./../font/YuGothB.ttc",width=1200,height=800)
 wc.generate_from_frequencies(word_freq)
 wc.to_file("./../plot/wordcloud.png")
 
