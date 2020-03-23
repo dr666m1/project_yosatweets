dcadbc7aa0528686c25fca6fdfdb6a29363cbd32
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-03-23 22:38:11 +0900

    routine

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ gcloud functions deploy yosatweets_count_tweets --entry-point main_count_tweets 
 
 gcloud functions deploy yosatweets_plot_line_chart --entry-point main_plot_line_chart --runtime python37 --trigger-http --memory 2048MB
 
-gcloud functions deploy yosatweets_plot_wordcloud --entry-point main_plot_wordcloud --runtime python37 --trigger-http --memory 2048MB
+gcloud functions deploy yosatweets_plot_wordcloud --entry-point main_plot_wordcloud --runtime python37 --trigger-http --memory 2048MB --timeout 500s
