b78004b233070ede76f9f13215abefcb0427d85e
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-04-10 22:53:13 +0900

    routine

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
-gcloud functions deploy yosatweets_insert_tweets --entry-point main_insert_tweets --runtime python37 --trigger-http --memory 2048MB --ingress-settings internal-only --allow-unauthenticated
+gcloud functions deploy yosatweets_insert_tweets --entry-point main_insert_tweets --runtime python37 --trigger-http --memory 2048MB --timeout 500s --ingress-settings internal-only --allow-unauthenticated
 
 gcloud functions deploy yosatweets_count_tweets --entry-point main_count_tweets --runtime python37 --trigger-http --memory 2048MB --ingress-settings internal-only --allow-unauthenticated
 
